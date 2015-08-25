9387a0d2919a3d3bec9c340039b007f99f60a5dd
Stefan Knott <stkn5801@colorado.edu>
2015-08-25 11:43:03 -0600

    comment handling

--- jfmt.py
+++ jfmt.py
@@ -3,11 +3,25 @@ import sys, re, fileinput
 if __name__ == "__main__":
 	filename = sys.argv[1]
 	indents = 0
+	comment = False #flag for indicating within a comment
 
 	#stdout redirected to file
 	for line in fileinput.input(filename, inplace=True):
 		leading_spc = len(line) - len(line.lstrip())
 	
+		#check for comments...
+		if re.search("/\*", line):
+			comment = True
+
+		if re.search("\*/", line):
+			comment = False
+			print indents * 4 * " " + line.strip()
+			continue
+
+		if comment:
+			print indents * 4 * " " + line.strip()
+			continue
+
 		#found close of scope
 		if re.search("\s*\S*\}", line):
 			indents -= 1
