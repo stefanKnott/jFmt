8d1add12af019fafee8cb42ae81ee2c4a4b9e990
Stefan Knott <stkn5801@colorado.edu>
2015-10-14 14:04:14 -0600

    example java with horrible spacing..

---
+++ example.java
@@ -0,0 +1,19 @@
+class PublicClass{
+    /*OMG
+Line me up!
+    */
+    
+    	public static void main(String []args){
+        int i = 0;
+   	     double z = 2;
+    }
+    
+    public method1(){
+        //some method operations
+        //op1
+         //op2
+        	if (conditional){
+	            fmtME!!
+        		}
+  }
+}
